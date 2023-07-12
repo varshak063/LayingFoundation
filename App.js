86afbcc36bf67bc675821e7ed3c386d7a1754c32
unknown <varshak063@gmail.com>
2023-07-12 18:11:42 +0530

    chapter3

--- App.js
+++ App.js
@@ -2,10 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React ELement =>render =>Object=>HTML element
-const heading = React.createElement(
-  "h1",
-  { id: "headings" },
-  "Hi Welcome to React"
-);
+
+// JSX not HTML in JS
+/**
+ * JSX Babel transpiles it to React.Element =>ReactElement Object=>HTML ELment
+ * JSX transpilled before it reaches to JS engine by parcel Babel is JS compiler which converts
+ */
+const heading = <h1 id="headings">React JSX syntax</h1>;
+
+//React Components
+/**
+ * 2 types
+ * a) Functional Components -New
+ * b) Class Based Component -OLD
+ */
+
+// Function Based Components
+
+const Title = () => <h1>This is Title Component</h1>;
+//component Composition : Keeping Component inside another component Title is called inside Heading component
+const HeadingComponent = () => {
+  return (
+    <>
+      <Title />
+      <h1>Hi Welcome!!</h1>
+      <h3 id="headings">This is function Component</h3>
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
